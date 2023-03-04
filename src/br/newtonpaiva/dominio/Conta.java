00792d783b9ca1696f129814015c1f57c73bdcf5
Paulo <paulohesilva@hotmail.com>
2023-03-03 21:21:42 -0300

    feat: Aula 03

--- src/br/newtonpaiva/dominio/Conta.java
+++ src/br/newtonpaiva/dominio/Conta.java
@@ -1,16 +1,35 @@
 package br.newtonpaiva.dominio;
 
+import java.util.Objects;
+
 public class Conta {
     private Integer numero;
 
     private Double saldo;
+
+    public Conta() {
+        this(null, null);
+    }
+
+    public Conta(Integer numero) {
+        this(numero, 0.0);
+    }
+
+    public Conta(Integer numero, Double saldo) {
+        this.numero = numero;
+        this.saldo = saldo;
+    }
 
     public Double sacar(Double valor) {
         return 0.0;
     }
 
     public Double depositar(Double valor) {
-        return 0.0;
+        if (valor == null || valor <= 0)
+            throw new IllegalArgumentException("Valor menor ou");
+
+        saldo += valor;
+        return saldo;
     }
 
     public Integer getNumero() {
@@ -21,5 +40,22 @@ public class Conta {
         this.numero = numero;
     }
 
+    public Double getSaldo() {
+        return saldo;
+    }
 
+    public void setSaldo(Double saldo) { this.saldo = saldo; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Conta conta = (Conta) o;
+        return numero.equals(conta.numero);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numero);
+    }
 }
